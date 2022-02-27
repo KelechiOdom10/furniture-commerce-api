c6b4f62bc505a60b925c60b62851dc5f75209e1a
Kelechiodom10 <kelechiodom10@gmail.com>
2022-02-27 00:34:33 +0000

    Created category repository and interface

--- Interfaces/ICategoryRepository.cs
+++ Interfaces/ICategoryRepository.cs
@@ -6,6 +6,7 @@ namespace API.Interfaces
     {
         Task<IReadOnlyList<Category>> GetCategoriesAsync();
         Task<Category> GetCategoryByIdAsync(Guid id);
+        Task<Category> GetCategoryByNameAsync(string name);
         Task<bool> AddCategoryAsync(Category category);
         Task<bool> UpdateCategoryAsync(Category category);
         Task<bool> DeleteCategoryByIdAsync(Guid id);
