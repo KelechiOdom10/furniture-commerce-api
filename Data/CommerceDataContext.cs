3f5fc091e29a193156604aeab41731b95969aa7c
Kelechiodom10 <kelechiodom10@gmail.com>
2022-02-27 00:35:20 +0000

    Updated mmodel builder in data context

--- Data/CommerceDataContext.cs
+++ Data/CommerceDataContext.cs
@@ -2,24 +2,25 @@
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Data
+namespace API.Data;
+
+public class CommerceDataContext : DbContext
 {
-    public class CommerceDataContext : DbContext
-	{
-        public CommerceDataContext(DbContextOptions<CommerceDataContext> options) :
-            base(options)
-		{
-            Database.EnsureCreated();
-		}
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Category>().HasMany(c => c.Products).WithOne(p => p.Category).HasForeignKey(p => p.CategoryId);
-        }
-
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products { get; set; }
-
+    public CommerceDataContext(DbContextOptions<CommerceDataContext> options) :
+        base(options)
+    {
+        Database.EnsureCreated();
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
+        modelBuilder.Entity<Category>().HasMany(c => c.Products).WithOne(p => p.Category).HasForeignKey(p => p.CategoryId);
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
 }
+
 
