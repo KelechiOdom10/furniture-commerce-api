38d458af7f96831b7f7e42ac3454139c480be983
Kelechiodom10 <kelechiodom10@gmail.com>
2022-03-04 20:49:16 +0000

    Created product type and updated the relationships

--- Data/CommerceDataContext.cs
+++ Data/CommerceDataContext.cs
@@ -16,10 +16,14 @@ public class CommerceDataContext : DbContext
     {
         modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
         modelBuilder.Entity<Category>().HasMany(c => c.Products).WithOne(p => p.Category).HasForeignKey(p => p.CategoryId);
+        modelBuilder.Entity<Category>().HasMany(c => c.ProductTypes).WithOne(pt => pt.Category).HasForeignKey(pt => pt.CategoryId);
+        modelBuilder.Entity<ProductType>().HasIndex(pt => pt.Name).IsUnique();
+        modelBuilder.Entity<ProductType>().HasMany(pt => pt.Products).WithOne(p => p.ProductType).HasForeignKey(p => p.ProductTypeId);
     }
 
-    public DbSet<Category> Categories { get; set; }
-    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<ProductType> ProductTypes => Set<ProductType>();
 
 }
 
