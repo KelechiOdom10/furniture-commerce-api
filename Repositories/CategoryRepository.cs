ca747d4468240e8b1b69a0d6b701f6985121589a
Kelechiodom10 <kelechiodom10@gmail.com>
2022-03-10 23:14:00 +0000

    Included relationships when returning data from db

--- Repositories/CategoryRepository.cs
+++ Repositories/CategoryRepository.cs
@@ -4,56 +4,66 @@ using API.Interfaces;
 using API.Entities;
 using API.Data;
 
-namespace API.Repositories
+namespace API.Repositories;
+
+public class CategoryRepository : ICategoryRepository
 {
-    public class CategoryRepository : ICategoryRepository
+    private readonly CommerceDataContext _context;
+    public CategoryRepository(CommerceDataContext context)
     {
-        private readonly CommerceDataContext _context;
-        public CategoryRepository(CommerceDataContext context)
-        {
-            _context = context;
-        }
+        _context = context;
+    }
 
-        public async Task<Category> GetCategoryByIdAsync(Guid id)
-        {
-            return await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
-        }
+    public async Task<Category> GetCategoryByIdAsync(Guid id)
+    {
+        var category = await _context.Categories
+            .Include(c => c.Products)
+            .Include(c => c.ProductTypes)
+            .FirstOrDefaultAsync(c => c.Id == id);
+        return category;
+    }
 
-        public async Task<IReadOnlyList<Category>> GetCategoriesAsync()
-        {
-            return await _context.Categories.ToListAsync();
-        }
+    public async Task<IReadOnlyList<Category>> GetCategoriesAsync()
+    {
+        var categories = await _context.Categories
+            .Include(c => c.Products)
+            .ToListAsync();
+        return categories;
+    }
 
-        public async Task<Category> GetCategoryByNameAsync(string name)
-        {
-            return await _context.Categories.FirstOrDefaultAsync(c => c.Name == name);
-        }
+    public async Task<Category> GetCategoryByNameAsync(string name)
+    {
+        var category = await _context.Categories
+            .Include(c => c.Products)
+            .Include(c => c.ProductTypes)
+            .FirstOrDefaultAsync(c => c.Name == name);
+        return category;
+    }
 
-        public async Task<bool> AddCategoryAsync(Category category)
-        {
-            await _context.Categories.AddAsync(category);
-            return await SaveAsync();
-        }
+    public async Task<bool> AddCategoryAsync(Category category)
+    {
+        await _context.Categories.AddAsync(category);
+        return await SaveAsync();
+    }
 
-        public async Task<bool> UpdateCategoryAsync(Category category)
-        {
-            _context.Categories.Update(category);
-            return await SaveAsync();
-        }
+    public async Task<bool> UpdateCategoryAsync(Category category)
+    {
+        _context.Categories.Update(category);
+        return await SaveAsync();
+    }
 
-        public async Task<bool> DeleteCategoryByIdAsync(Guid id)
-        {
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
-                return false;
+    public async Task<bool> DeleteCategoryByIdAsync(Guid id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+        if (category == null)
+            return false;
 
-            _context.Categories.Remove(category);
-            return await SaveAsync();
-        }
+        _context.Categories.Remove(category);
+        return await SaveAsync();
+    }
 
-        private async Task<bool> SaveAsync()
-        {
-            return await _context.SaveChangesAsync() > 0;
-        }
+    private async Task<bool> SaveAsync()
+    {
+        return await _context.SaveChangesAsync() > 0;
     }
 }
