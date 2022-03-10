9da393305f6776e3573110e3da347a4342fd86e3
Kelechiodom10 <kelechiodom10@gmail.com>
2022-03-10 23:00:53 +0000

    Converted to file-scoped namespace

--- Interfaces/ICategoryRepository.cs
+++ Interfaces/ICategoryRepository.cs
@@ -1,14 +1,13 @@
 using API.Entities;
 
-namespace API.Interfaces
+namespace API.Interfaces;
+
+public interface ICategoryRepository
 {
-    public interface ICategoryRepository
-    {
-        Task<IReadOnlyList<Category>> GetCategoriesAsync();
-        Task<Category> GetCategoryByIdAsync(Guid id);
-        Task<Category> GetCategoryByNameAsync(string name);
-        Task<bool> AddCategoryAsync(Category category);
-        Task<bool> UpdateCategoryAsync(Category category);
-        Task<bool> DeleteCategoryByIdAsync(Guid id);
-    }
+    Task<IReadOnlyList<Category>> GetCategoriesAsync();
+    Task<Category> GetCategoryByIdAsync(Guid id);
+    Task<Category> GetCategoryByNameAsync(string name);
+    Task<bool> AddCategoryAsync(Category category);
+    Task<bool> UpdateCategoryAsync(Category category);
+    Task<bool> DeleteCategoryByIdAsync(Guid id);
 }
--- Interfaces/IProductTypeRepository.cs
+++ Interfaces/IProductTypeRepository.cs
@@ -1,14 +1,13 @@
 using API.Entities;
 
-namespace API.Interfaces
+namespace API.Interfaces;
+
+public interface IProductTypeRepository
 {
-    public interface IProductTypeRepository
-    {
-        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
-        Task<ProductType> GetProductTypeByIdAsync(Guid id);
-        Task<ProductType> GetProductTypeByNameAsync(string name);
-        Task<bool> AddProductTypeAsync(ProductType productType);
-        Task<bool> UpdateProductTypeAsync(ProductType productType);
-        Task<bool> DeleteProductTypeByIdAsync(Guid id);
-    }
+    Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
+    Task<ProductType> GetProductTypeByIdAsync(Guid id);
+    Task<ProductType> GetProductTypeByNameAsync(string name);
+    Task<bool> AddProductTypeAsync(ProductType productType);
+    Task<bool> UpdateProductTypeAsync(ProductType productType);
+    Task<bool> DeleteProductTypeByIdAsync(Guid id);
 }
