44124ecba64f24f8b67b040e9f1145f8012c4e4d
Kelechiodom10 <kelechiodom10@gmail.com>
2022-03-26 15:41:20 +0000

    Created new migrations for seeding and creating product functionality

--- Data/CommerceDataContext.cs
+++ Data/CommerceDataContext.cs
@@ -1,5 +1,6 @@
 ﻿using API.Entities;
 using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 namespace API.Data;
 
@@ -14,10 +15,19 @@ public class CommerceDataContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
+        modelBuilder.Entity<Category>().HasIndex(p => p.Slug).IsUnique();
         modelBuilder.Entity<Category>().HasMany(c => c.Products).WithOne(p => p.Category).HasForeignKey(p => p.CategoryId);
         modelBuilder.Entity<Category>().HasMany(c => c.ProductTypes).WithOne(pt => pt.Category).HasForeignKey(pt => pt.CategoryId);
+
         modelBuilder.Entity<ProductType>().HasIndex(pt => pt.Name).IsUnique();
+        modelBuilder.Entity<ProductType>().HasIndex(pt => pt.Slug).IsUnique();
         modelBuilder.Entity<ProductType>().HasMany(pt => pt.Products).WithOne(p => p.ProductType).HasForeignKey(p => p.ProductTypeId);
+
+        modelBuilder.Entity<Product>().HasIndex(p => p.Slug).IsUnique();
+        modelBuilder.Entity<Product>().HasIndex(p => p.Name).IsUnique();
+
+        modelBuilder.Seed();
+
     }
 
     public DbSet<Category> Categories => Set<Category>();
